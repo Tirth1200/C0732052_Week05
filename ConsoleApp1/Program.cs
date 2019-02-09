f45f729b6ba083afb5800ceb9a066ebab3f4c158
visualstudio <visualstudio@122-PC15>
2019-02-08 19:06:11 -0500

    07:00 PM commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             elevator e = new elevator();
-            Console.Read();
+            e.run();
         }
     }
     class Node
@@ -33,9 +33,22 @@ namespace ConsoleApp1
             FirstFloor = new Node();
             FirstFloor.FloorNumber = "First Floor";
             Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
+
+            SecondFloor = new Node();
             FirstFloor.elevatorUp = SecondFloor;
             SecondFloor.FloorNumber = "Second Floor";
             Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
+
+            ThirdFloor = new Node();
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "Third Floor";
+            Console.WriteLine("floor number is {0} ", ThirdFloor.FloorNumber);
+
+            FourthFloor = new Node();
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth Floor";
+            Console.WriteLine("floor number is {0} ", FourthFloor.FloorNumber);
+            FourthFloor.elevatorUp = null;
 
         }
     }
